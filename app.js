dfd6a2070eecfad22bad7ba79d8cface358d3ccd
venkateshwarluk <ksrinuvenkatesh@gmail.com>
2016-11-07 15:00:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+var E1 = {
+	name: "Raj",
+	age: "25 Years",
+	address: {
+		city: "Bengalore",
+		state: "Karnataka",
+		pinCode: "602026"
+	}
+};
+
+var E2 = {
+	name: "Yuva",
+	age: "21 Years",
+	address: {
+		city: "Uthukottai",
+		state: "TamilNadu",
+		pinCode: "602026"
+	}
+};
+
+var E3 = {
+	name: "Venki",
+	age: "30 Years",
+	address: {
+		city: "Bengalore",
+		state: "Karnataka",
+		pinCode: "602026"
+	}
+};
+
+var E4 = {
+	name: "Narendhar",
+	age: "80 Years",
+	address: {
+		city: "Kochi",
+		state: "Kerala",
+		pinCode: "576905"
+	}
+};
+
+var E5 = {
+	name: "Abi",
+	age: "90 Years",
+	address: {
+		city: "Usmango",
+		state: "Aprica",
+		pinCode: "602026"
+	}
+};
+
+var employee = [E1, E2, E3, E4, E5]
+
+console.log(employee[1].name);
+console.log(employee[1].age);
+console.log(employee[1].address.city);
+console.log(employee[1].address.state);
+console.log(employee[1].address.pinCode);
